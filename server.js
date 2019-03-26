75cf8f83590a5aaea6700963d0d557a1f713ea7d
Rohidas <rohidas.gijare@smellycat.in>
2019-03-26 19:17:20 +0530

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');  
-const app = express();  
-app.use(express.static(__dirname + '/dist'));  
-app.all('*', (req, res) => {  
-  res.status(200).sendFile(__dirname + '/dist/index.html');  
-});  
-app.listen(process.env.PORT || 8080); 
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + '/dist/mdb-angular-free'));
+app.all('*', (req, res) => {
+  res.status(200).sendFile(__dirname + '/dist/mdb-angular-free/index.html');
+});
+app.listen(process.env.PORT || 3201);
