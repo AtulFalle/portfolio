bbb9dbba58784c618a6fc8aa0a953f31d869a324
atul <atulfalle007@gmail.com>
2019-03-27 18:59:54 +0530

    server

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-app.use(express.static(__dirname + '/dist/mdb-angular-free'));
-app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/dist/mdb-angular-free/index.html');
+app.use(express.static(__dirname + '/angularapp'));
+ 
+app.get('/*', function(req,res) {
+ 
+res.sendFile(path.join(__dirname+'/angularapp/index.html'));
 });
-app.listen(process.env.PORT || 8080);
